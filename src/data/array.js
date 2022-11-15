d8c29fd4a95620121cb9a46149fee8f882872639
theborland <borland2000@hotmail.com>
2022-11-15 09:35:28 -0500

    Update array.js

--- src/data/array.js
+++ src/data/array.js
@@ -1,5 +1,25 @@
 module.exports = [
-  {
+   {
+    title: 'Array-1',
+    name: 'firstLast6',
+    inputs: [
+      '([1, 2, 6])',
+      '([6, 1, 2, 3])',
+      '([13, 6, 1, 2, 3])',
+      '([13, 6, 1, 2, 6])',
+      '([3, 2, 1])',
+      '([3, 6, 1])',
+      '([3, 6])',
+      '([6])',
+      '([3])',
+      '([5, 6])',
+      '([5, 5])',
+      '([1, 2, 3, 4, 6])',
+      '([1, 2, 3, 4])',
+    ],
+    question: `Given an array of ints, return true if 6 appears as either the first or
+    last element in the array. The array will be length 1 or more.`,
+  }, {
     title: 'Array-1',
     name: 'firstLast6',
     inputs: [
